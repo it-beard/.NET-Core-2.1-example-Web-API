1d06d7634a590b588fae8c2efc5d60905af65278
Alex Kartynnik <alexey.kartynnik@billwerk.com>
2020-03-15 00:01:07 +0300

    Initial commit

---
+++ Itbeard.Models/Url/UrlModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Itbeard.Models
+{
+    public class UrlModel
+    {
+        public Guid Id { get; set; }
+        
+        public string TargetUrl { get; set; }
+        
+        public string ShortGuid { get; set; }
+    }
+}
